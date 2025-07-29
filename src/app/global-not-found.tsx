714acb29dde7a0bf068cffec2a338b905b8a76b5
Thomas Sjerps <sjerpsthomas@gmail.com>
2025-07-30 01:21:41 +0200

    lowercase 404 is better

--- src/app/global-not-found.tsx
+++ src/app/global-not-found.tsx
@@ -10,8 +10,8 @@ export default async function NotFoundPage() {
   return (
     <html>
       <body className={twMerge(font.className, 'mt-36')}>
-        <h1 className='rotate-[4deg] text-8xl mb-8 w-fit mx-auto'>Oops!</h1>
-        <p className='-rotate-2 text-3xl w-fit mx-auto'>This page does not exist.</p>
+        <h1 className='rotate-[4deg] text-8xl mb-8 w-fit mx-auto'>oops!</h1>
+        <p className='-rotate-2 text-3xl w-fit mx-auto'>this page does not exist.</p>
       </body>
     </html>
   );

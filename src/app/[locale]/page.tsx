aa4ab690ea380a6d061aa803e407bd3851bf7bc4
Thomas Sjerps <sjerpsthomas@gmail.com>
2025-08-06 18:10:30 +0200

    Add initial home page content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,45 @@
 import {Header} from "@/components/header/Header";
 import {Locale} from "@/i18n/routing";
+import {BulletPointList} from "@/components/BulletPointList";
+import {Block} from "@/components/Block";
+
+
+const D = {
+  nl: {
+    texts: [
+      "Hoi!",
+      "Welkom bij mijn website. Hier kan je m'n CV en portfolio vinden.",
+      "Ik heb nog een paar plannen voor de site:",
+    ],
+    todo: [
+      "Spelletjespagina",
+      "De Next router fixen (hij doet raar bij het wisselen van taal)",
+      "Turbopack fixen (local hot reload werkt niet meer)",
+      "Fotopagina?",
+    ]
+  },
+  en: {
+    texts: [
+      "Hi!",
+      "Welcome to my website. Here you can find my CV and portfolio.",
+      "I have a few more plans for the site:",
+    ],
+    todo: [
+      "Games page",
+      "Fix the Next router (it's acting strangely when switching languages)",
+      "Fix Turbopack (local hot reload isn't working anymore)",
+      "Photo page?",
+    ]
+  },
+}
 
 
 export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
   // Get locale
   const locale = (await params).locale as Locale;
+
+  // Get dictionary
+  const dict = D[locale];
 
   return (
     <>
@@ -13,7 +48,13 @@ export default async function HomePage({ params }: { params: Promise<{ locale: s
 
       {/* Content */}
       <main>
-        {/* TODO */}
+        <Block>
+          <h1>{dict.texts[0]}</h1>
+
+          <p>{dict.texts[1]}</p>
+          <p>{dict.texts[2]}</p>
+          <BulletPointList content={dict.todo}/>
+        </Block>
       </main>
     </>
   );

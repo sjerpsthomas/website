0636e958b92da3d7fd7310d20a565fa0a4173144
Thomas Sjerps <sjerpsthomas@gmail.com>
2025-09-09 20:58:04 +0200

    Move home page to MDX

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,33 +1,15 @@
 import {Header} from "@/components/header/Header";
-import {BulletPointList} from "@/components/BulletPointList";
 import {Block} from "@/components/Block";
 import {Locale} from "@/api/locale";
-
+import ContentEN from "./content-en.mdx";
+import ContentNL from "./content-nl.mdx";
 
 const D = {
   nl: {
-    texts: [
-      "Hoi!",
-      "Welkom bij mijn website. Hier kan je m'n CV en portfolio vinden.",
-      "Ik heb nog een paar plannen voor de site:",
-    ],
-    todo: [
-      "Spelletjespagina",
-      "Fotopagina?",
-    ]
   },
   en: {
-    texts: [
-      "Hi!",
-      "Welcome to my website. Here you can find my CV and portfolio.",
-      "I have a few more plans for the site:",
-    ],
-    todo: [
-      "Games page",
-      "Photo page?",
-    ]
-  },
-}
+  }
+};
 
 
 export default async function HomePage({ params }: { params: Promise<{ locale: string }> }) {
@@ -45,13 +27,8 @@ export default async function HomePage({ params }: { params: Promise<{ locale: s
       {/* Content */}
       <main>
         <div className='flex flex-col items-center'>
-
           <Block className='w-full md:w-[80%] md:min-w-[30rem]'>
-            <h1>{dict.texts[0]}</h1>
-
-            <p>{dict.texts[1]}</p>
-            <p>{dict.texts[2]}</p>
-            <BulletPointList content={dict.todo}/>
+            { locale == 'nl' ? <ContentNL/> : <ContentEN/> }
           </Block>
         </div>
       </main>
--- src/components/BulletPointList.tsx
+++ src/components/BulletPointList.tsx
@@ -1,12 +1,10 @@
 
 export function BulletPointList({ content }: { content: string[] }) {
   return (
-    <ul className='list-disc list-outside pl-5'>
-      {
-        content.map((item, i) =>
-          <li key={i}>{item}</li>
-        )
-      }
+    <ul>
+      {content.map((item, i) =>
+        <li key={i}>{item}</li>
+      )}
     </ul>
   );
 }
